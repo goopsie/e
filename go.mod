6f023bf3a128b2347285c2736f879adea90f0f15
Ulisse Mini <ulisse.mini@gmail.com>
2020-11-22 19:57:37 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/goopsie/e
+
+go 1.15
+
+require github.com/faiface/beep v1.0.2
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"io/ioutil"
+	"log"
+	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+)
+
+func waitForE() {
+	time.Sleep(time.Second * 5) // TODO
+}
+
+func playSound(streamer beep.StreamSeeker) {
+	log.Print("playing 3.mp3")
+	err := streamer.Seek(0)
+	if err != nil {
+		log.Print("seek", err)
+		return
+	}
+
+	speaker.Play(streamer)
+	if streamer.Err() != nil {
+		log.Print("streamer", streamer.Err())
+	}
+}
+
+// why am i forced to commit such sin by beep
+// it wants close() but bytes reader does not provide
+// i make fake :)))
+type bufCloser struct {
+	*bytes.Reader
+}
+
+func (b *bufCloser) Close() error { return nil }
+
+func main() {
+	// Initialize speaker and sound code.
+	soundBytesB64Buf := bytes.NewBuffer(soundBytesB64)
+	r := base64.NewDecoder(base64.StdEncoding, soundBytesB64Buf)
+	soundBytes, err := ioutil.ReadAll(r)
+	errp(err)
+	soundBytesBuf := &bufCloser{bytes.NewReader(soundBytes)}
+
+	streamer, format, err := mp3.Decode(soundBytesBuf)
+	errp(err)
+	defer streamer.Close()
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	defer speaker.Close()
+
+	for {
+		log.Print(streamer.Len())
+		playSound(streamer) // TODO: switch order
+		waitForE()
+	}
+}
+
+func errp(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
